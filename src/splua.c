04f1c857163f4abe09b915632e2d15cd326a96cc
fengidri <fengidri@gmail.com>
2016-08-22 01:38:41 +0000

    fix

--- src/splua.c
+++ src/splua.c
@@ -558,7 +558,7 @@ static int splua_scripts(lua_env_t *env, const char *dirpath, lua_State *L)
     {
         if (DT_REG != ptr->d_type) continue;
         if (strlen(ptr->d_name) < 5) continue;
-        if (strcmp(ptr->d_name[strlen(ptr->d_name) - 4], ".lua")) continue;
+        if (strcmp(ptr->d_name + strlen(ptr->d_name) - 4, ".lua")) continue;
         if (ptr->d_name[0] == '.') continue;
 
         if (strlen(dirpath) + strlen(ptr->d_name) + 10 > sizeof(path))
